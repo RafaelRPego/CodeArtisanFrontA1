169c13273c9089b35d9d98d5aa2b978af21be284
RafaelRPego <darakrex@gmail.com>
2024-05-02 23:27:42 -0300

    Home definition space

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,11 @@ if (navClose) {
         navMenu.classList.remove("show-menu");
     });
 }
+
+function scrollHeader () {
+    const header = document.getElementById("header");
+    if (this.scrollY >= 100) header.classList.add("scroll-header");
+    else header.classList.remove("scroll-header");
+}
+
+window.addEventListener("scroll", scrollHeader);
